0bb07ea599b60c5cf6b503d9b22b9998cedfd828
Vitor Nunes <vitor1nuness@gmail.com>
2025-10-21 18:42:15 -0300

    feat: link para seção sobre e para app no header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,7 @@
-<script setup></script>
+<script setup>
+import LinkButtonEatzy from './LinkButtonEatzy.vue';
+
+</script>
 
 <template>
     <header>
@@ -14,7 +17,7 @@
             <nav>
                 <ul>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">
+                        <a href="#about" class="nav-link">
                             SOBRE O EATZY
                         </a>
                     </li>
@@ -29,9 +32,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="https://eatzy-nine.vercel.app" class="nav-button">
-                            IR PARA O EATZY
-                        </a>
+                        <LinkButtonEatzy />
                     </li>
                 </ul>
             </nav>
@@ -41,10 +42,11 @@
 
 <style scoped>
 header {
-    position: fixed;
+    position: absolute;
     top: 0;
     right: 0;
     width: 100%;
+    z-index: 9999;
 }
 
 .header-container {
@@ -95,12 +97,5 @@ a:hover {
 
 .nav-link:hover > .highlight {
     color: red;
-}
-
-.nav-button, .nav-button:hover {
-    color: white;
-    background-color: red;
-    padding: 12px;
-    border-radius: 12px;
 }
 </style>
